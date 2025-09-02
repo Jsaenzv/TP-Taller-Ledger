7b79b9c3e06c1313b58f91820ae5b7d4f9a31fd9
Jsaenzv <jsaenz@fi.uba.ar>
2025-09-02 09:49:29 -0300

    primer commit

---
+++ lib/Ledger.ex
@@ -0,0 +1,18 @@
+defmodule Ledger do
+  @moduledoc """
+  Documentation for `Tp1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tp1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
