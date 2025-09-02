132c69ff4cfa108553161b8da93588fa78e5f2d1
Jsaenzv <jsaenz@fi.uba.ar>
2025-09-02 12:05:07 -0300

    agrego constante delimitador_csv

--- lib/Ledger.ex
+++ lib/Ledger.ex
@@ -1,5 +1,6 @@
 defmodule Ledger do
   @csv_path "./data/transacciones.csv"
+  @delimitador_csv ";"
   def main(["transacciones"]) do
     transacciones()
   end
@@ -8,7 +9,7 @@ defmodule Ledger do
     File.stream!(@csv_path)
     |> CSV.decode()
     |> Enum.each(fn
-      {:ok, fila} -> IO.puts(Enum.join(fila, ";"))
+      {:ok, fila} -> IO.puts(Enum.join(fila, @delimitador_csv))
       {:error, motivo} -> IO.puts("Error: #{motivo}")
     end
     )
