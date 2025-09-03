e88f85ac4165398e31dd3f095142df38e4226a9c
Jsaenzv <jsaenz@fi.uba.ar>
2025-09-03 09:44:17 -0300

    Agrego funcionalidad de flags a transacciones

--- lib/Ledger.ex
+++ lib/Ledger.ex
@@ -1,18 +1,62 @@
 defmodule Ledger do
+  @headers [
+    "id",
+    "timestamp",
+    "moneda_origen",
+    "moneda_destino",
+    "monto",
+    "cuenta_origen",
+    "cuenta_destino",
+    "tipo"
+  ]
   @csv_path "./data/transacciones.csv"
   @delimitador_csv ";"
-  def main(["transacciones"]) do
-    transacciones()
+
+  def main(["transacciones" | flags]) do
+    params = parsear_flags(flags)
+    transacciones(params)
   end
 
-  def transacciones() do
-    File.stream!(@csv_path)
-    |> CSV.decode()
-    |> Enum.each(fn
-      {:ok, fila} -> IO.puts(Enum.join(fila, @delimitador_csv))
-      {:error, motivo} -> IO.puts("Error: #{motivo}")
-    end
-    )
+  def parsear_flags(flags) do
+    Enum.map(flags, fn flag ->
+      String.split(flag, "=", parts: 2)
+      |> List.to_tuple()
+    end)
+    |> Enum.map(fn {clave, valor} ->
+      case {clave, valor} do
+        {"-c1", valor} -> {"cuenta_origen", valor}
+        {"-c2", valor} -> {"cuenta_destino", valor}
+        {"-t", valor} -> {:t, valor}
+        {"-o", valor} -> {:o, valor}
+        {"-m", valor} -> {:m, valor}
+        _ -> raise ArgumentError, message: "Flag no soportado: #{clave}"
+      end
+    end)
+    |> Map.new()
   end
 
+  def transacciones(params) do
+    path = Map.get(params, :t, @csv_path)
+    params_filtrados = Map.delete(params, :t)
+
+    filas_sin_filtrar =
+      path
+      |> File.stream!()
+      |> CSV.decode!(headers: true, separator: ?;)
+
+    filas_filtradas =
+      if map_size(params_filtrados) == 0 do
+        filas_sin_filtrar
+      else
+        filas_sin_filtrar
+        |> Enum.filter(fn fila ->
+          Enum.all?(params_filtrados, fn {flag, valor} -> fila[flag] == valor end)
+        end)
+      end
+
+    Enum.each(filas_filtradas, fn fila ->
+      valores = Enum.map(@headers, &fila[&1])
+      IO.puts(Enum.join(valores, @delimitador_csv))
+    end)
+  end
 end
--- test/ledger_test.exs
+++ test/ledger_test.exs
@@ -2,13 +2,17 @@ defmodule LedgerTest do
   use ExUnit.Case
   doctest Ledger
 
-  @ejecutable Path.join(File.cwd!(), "ledger")
-  @csv_path "./data/transacciones.csv"
+  @ejecutable_path Path.join(File.cwd!(), "ledger")
+  @data_test_path "./test/data/test.csv"
+  @data_test_csv "id;timestamp;moneda_origen;moneda_destino;monto;cuenta_origen;cuenta_destino;tipo\n12;1757600000;USDT;ETH;50.00;userA;userB;transferencia\n13;1757610000;ETH;USDT;1.25;userL;;swap\n14;1757620000;BTC;;1500.00;userM;;alta_cuenta\n15;1757630000;USDT;BTC;200.00;userA;userC;transferencia\n16;1757640000;BTC;BTC;0.30;userB;userC;transferencia"
 
-  test "ledger transacciones" do
-    {output, status} = System.cmd(@ejecutable, ["transacciones"])
-    assert status == 0
-    esperado = File.read!(@csv_path)
-    assert String.trim_trailing(output, "\n") == String.trim_trailing(esperado, "\n") # trim_trailing elimina "\n" del final si es que esta, caso contrario devuelve el mismo string
+  test "ledger transacciones con path" do
+    assert File.exists?(@ejecutable_path), "Compila el escript con: mix escript.build"
+    File.write!(@data_test_path, @data_test_csv)
+    {output, status} = System.cmd(@ejecutable_path, ["transacciones", "-t=#{@data_test_path}"])
+    assert status == 0, "El exit code de ejecutar el programa no fue exitoso."
+    esperado = @data_test_csv |> String.split("\n", parts: 2) |> List.last()
+    # trim_trailing elimina "\n" del final si es que esta, caso contrario devuelve el mismo string
+    assert String.trim_trailing(output, "\n") == String.trim_trailing(esperado, "\n")
   end
 end
