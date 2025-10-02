d7a6e56321801535fcd8ae4643b11cdae67ccd25
Jsaenzv <jsaenz@fi.uba.ar>
2025-10-02 10:30:38 -0300

    Creo tabla y schema Usuario

---
+++ lib/Ledger/Entidades/Usuario.ex
@@ -0,0 +1,17 @@
+defmodule Ledger.Entidades.Usuario do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :nombre, :string
+    field :fecha_nacimiento, :date
+    timestamps()
+  end
+
+  def changeset(user, atributos) do
+    user
+    |> cast(atributos, [:nombre, :fecha_nacimiento])
+    |> validate_required([:nombre, :fecha_nacimiento])
+    |> unique_constraint([:nombre])
+  end
+end
