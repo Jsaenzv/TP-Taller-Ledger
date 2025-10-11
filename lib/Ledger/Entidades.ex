88078d291372ae657990eb6c8e3873a4504eaec7
Jsaenzv <jsaenz@fi.uba.ar>
2025-10-11 11:45:02 -0300

    Agrego función eliminar usuario con tests

--- lib/Ledger/Entidades.ex
+++ lib/Ledger/Entidades.ex
@@ -5,11 +5,14 @@ defmodule Ledger.Entidades do
 
   def crear_usuario(nombre, fecha_nacimiento) do
     atributos = %{nombre: nombre, fecha_nacimiento: fecha_nacimiento}
-    case Usuario.changeset(%Usuario{}, atributos) |> Repo.insert() do
-      {:ok, usuario} -> {:ok, usuario}
-      {:error, changeset} ->
-        error = FuncionesDB.errores_en(changeset)
-        {:error, inspect(error)}
-    end
+    Usuario.changeset(%Usuario{}, atributos) |> Repo.insert()
+  end
+
+  def editar_usuario(usuario, atributos) do
+      Usuario.changeset(usuario, atributos) |> Repo.update()
+  end
+
+  def eliminar_usuario(usuario) do
+    Repo.delete(usuario)
   end
 end
--- test/Ledger/entidades_test.exs
+++ test/Ledger/entidades_test.exs
@@ -14,17 +14,56 @@ defmodule Ledger.EntidadesTests do
   :ok
 end
 
+@nombre_default "juan"
+@nombre_alternativo "pedro"
+@fecha_nacimiento_default ~D[1990-01-01]
+@fecha_nacimiento_alternativa ~D[2000-01-01]
+@fecha_nacimiento_invalida ~D[2020-01-01]
+
 
   describe "crear_usuario/2" do
     test "Ingreso usuario válido" do
-      usuario =
-      %Usuario{nombre: "juan", fecha_nacimiento: ~D[1990-01-01]}
+      {:ok, usuario_creado} = Entidades.crear_usuario(@nombre_default, @fecha_nacimiento_default)
+      assert usuario_creado.nombre == @nombre_default
+      assert usuario_creado.fecha_nacimiento == @fecha_nacimiento_default
 
-      {:ok, usuario_creado} = Entidades.crear_usuario("juan", ~D[1990-01-01])
-      assert usuario_creado.nombre == usuario.nombre
-      assert usuario_creado.fecha_nacimiento == usuario.fecha_nacimiento
+    end
+
+    test "Ingreso usuario inválido" do
+      assert {:error, changeset} = Entidades.crear_usuario(nil, nil)
+      refute changeset.valid?
+      assert %{nombre: ["Este campo es obligatorio"], fecha_nacimiento: ["Este campo es obligatorio"]} = FuncionesDB.errores_en(changeset)
+
+      assert {:error, changeset} = Entidades.crear_usuario(@nombre_default, @fecha_nacimiento_invalida)
+      refute changeset.valid?
+      assert %{fecha_nacimiento: ["El usuario debe ser mayor de 18 años"]} = FuncionesDB.errores_en(changeset)
 
     end
   end
 
+  describe "editar_usuario/2" do
+
+    test "Edito usuario" do
+      {:ok, usuario_creado} = Entidades.crear_usuario(@nombre_default, @fecha_nacimiento_default)
+      atributos = %{nombre: @nombre_alternativo, fecha_nacimiento: @fecha_nacimiento_alternativa}
+      {:ok, usuario_editado} = Entidades.editar_usuario(usuario_creado, atributos)
+      assert usuario_editado.nombre == @nombre_alternativo
+      assert usuario_editado.fecha_nacimiento == @fecha_nacimiento_alternativa
+
+    end
+  end
+
+  describe "eliminar_usuario/1" do
+    test "elimino usuario válido" do
+      {:ok, usuario_creado} = Entidades.crear_usuario(@nombre_default, @fecha_nacimiento_default)
+      assert {:ok, usuario_eliminado} = Entidades.eliminar_usuario(usuario_creado)
+      assert usuario_eliminado.id == usuario_creado.id
+      assert Repo.get(Usuario, usuario_creado.id) == nil
+    end
+
+    test "elimino usuario inexistente en la base de datos" do
+      usuario_falso = %Usuario{id: -1, nombre: "fantasma", fecha_nacimiento: ~D[1980-01-01]}
+      assert {:error, :not_found} = Entidades.eliminar_usuario(usuario_falso)
+    end
+  end
 end
