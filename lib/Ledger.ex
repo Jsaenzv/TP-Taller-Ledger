73774e51f86edeed774027478dbcfe80f1722d34
Jsaenzv <jsaenz@fi.uba.ar>
2025-10-13 11:31:21 -0300

    paso lógica del módulo Ledger al CSV

--- lib/Ledger.ex
+++ lib/Ledger.ex
@@ -92,22 +92,9 @@ defmodule Ledger do
     moneda = Map.get(params, "moneda", Constantes.default_moneda())
     params = Map.delete(params, "moneda")
 
-    tipos_de_cambio =
-      case CSV.leer_csv(Constantes.csv_monedas_path(), Constantes.headers_moneda()) do
-        {:ok, tipos_de_cambio} ->
-          Enum.reduce(tipos_de_cambio, %{}, fn %{"moneda" => moneda, "cambio" => cambio}, acc ->
-            Map.put(acc, moneda, Parser.parse_float(cambio))
-          end)
+    tipos_de_cambio = CSV.obtener_tipos_de_cambio()
 
-        {:error, razon} ->
-          raise(razon)
-      end
-
-    transacciones =
-      case CSV.leer_csv(input_path, Constantes.headers_transacciones()) do
-        {:ok, filas} -> filas
-        {:error, razon} -> raise(razon)
-      end
+    transacciones = CSV.obtener_transacciones(input_path)
 
     case Validador.validar_transacciones(transacciones, tipos_de_cambio) do
       {:error, razon} -> raise(razon)
--- lib/Ledger/CSV.ex
+++ lib/Ledger/CSV.ex
@@ -1,5 +1,6 @@
 defmodule Ledger.CSV do
   alias Ledger.Constantes
+  alias Ledger.Parser
 
   def leer_csv(path, headers) do
     case File.exists?(path) do
@@ -24,5 +25,24 @@ defmodule Ledger.CSV do
       false ->
         {:error, "Archivo no encontrado: #{path}"}
     end
+  end
+
+  def obtener_tipos_de_cambio(input_path \\ Constantes.csv_monedas_path) do
+    case leer_csv(input_path, Constantes.headers_moneda()) do
+        {:ok, tipos_de_cambio} ->
+          Enum.reduce(tipos_de_cambio, %{}, fn %{"moneda" => moneda, "cambio" => cambio}, acc ->
+            Map.put(acc, moneda, Parser.parse_float(cambio))
+          end)
+
+        {:error, razon} ->
+          raise(razon)
+      end
+  end
+
+  def obtener_transacciones(input_path \\ Constantes.csv_transacciones_path()) do
+    case leer_csv(input_path, Constantes.headers_transacciones()) do
+        {:ok, filas} -> filas
+        {:error, razon} -> raise(razon)
+      end
   end
 end
