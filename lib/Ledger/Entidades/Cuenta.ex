878bfd075412fe4ec48e14304b8a7c173cc396a6
Jsaenzv <jsaenz@fi.uba.ar>
2025-10-17 16:31:47 -0300

    Modifico tabla cuentas para que incluya el campo balance

--- lib/Ledger/Entidades/Cuenta.ex
+++ lib/Ledger/Entidades/Cuenta.ex
@@ -1,18 +1,21 @@
 defmodule Ledger.Entidades.Cuenta do
   use Ecto.Schema
   import Ecto.Changeset
-  alias Ledger.Entidades.{Usuario, Moneda}
+  alias Ledger.Entidades.{Moneda, Usuario}
 
   schema "cuentas" do
     belongs_to(:usuario, Usuario)
     belongs_to(:moneda, Moneda)
+    field(:balance, :float, default: 0.0)
     timestamps()
   end
 
   def changeset(cuenta, attrs) do
     cuenta
-    |> cast(attrs, [:usuario_id, :moneda_id])
-    |> validate_required([:usuario_id, :moneda_id], message: "Este campo es obligatorio")
+    |> cast(attrs, [:usuario_id, :moneda_id, :balance])
+    |> validate_required([:usuario_id, :moneda_id, :balance],
+      message: "Este campo es obligatorio"
+    )
     |> unique_constraint([:usuario_id, :moneda_id], name: :cuentas_usuario_id_moneda_id)
   end
 end
